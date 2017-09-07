f2321387f193bc6493f95d328414dcbbcaf52ef2
홍유정 <youjung.hong@nhnent.com>
2017-09-07 22:36:34 +0900

    Revert "feat: register npm (resolves: #1) (#7)"
    This reverts commit 21cbb7b283b38e98c5f1333ec0fa7d7940d35ac8.

---
+++ src/index.js
@@ -0,0 +1,7 @@
+/**
+ * @namespace tui.component.animation
+ * @description Animation library
+ */
+import util from 'code-snippet';
+import * as animation from './anim';
+util.defineNamespace('tui.component.animation', animation);
